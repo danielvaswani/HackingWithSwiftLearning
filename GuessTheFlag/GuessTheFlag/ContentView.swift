6356bfa2f860351fc22ad4d435bb2240b14cdb82
Daniel Vaswani <vaswanidaniel@gmail.com>
2021-08-12 17:45:05 +0700

    complete GuessTheFlag Challenges

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -18,7 +19,7 @@ struct ContentView: View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
-            VStack {
+            VStack(spacing : 30){
                    Text("Tap the flag of")
                     .foregroundColor(.white)
                    Text(countries[correctAnswer])
@@ -37,11 +38,18 @@ struct ContentView: View {
                             .shadow(color: .black, radius: 2)
                     }
                 }
+                
+                // Challenge 2 show score under flags
+                Text("Score: \(score)")
+                    .bold()
+                    .foregroundColor(.white)
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            //Challenge 1 add score "Your score is \(score)"
+            //Challenge 3 show correct answer
+            Alert(title: Text(scoreTitle), message: Text("Wrong! That’s the flag of \(countries[correctAnswer])"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
@@ -50,6 +58,8 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            //Challenge 1 add score
+            score += 1
         } else {
             scoreTitle = "Wrong"
         }
