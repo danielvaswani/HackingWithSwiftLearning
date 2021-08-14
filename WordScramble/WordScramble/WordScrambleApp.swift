b1bd31a060e6ac7994739e19d2cc3bc173ca3ac9
Daniel Vaswani <vaswanidaniel@gmail.com>
2021-08-14 13:18:03 +0700

    add WordScramble, test personal access token

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Daniel Vaswani on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
