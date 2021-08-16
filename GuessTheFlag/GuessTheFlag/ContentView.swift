1401865b3e8f9b8c4a45df6394f8e31c85a5b1a0
Daniel Vaswani <vaswanidaniel@gmail.com>
2021-08-16 23:55:46 +0700

    completed Animation project challenges in GuessTheFlag

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -14,24 +14,43 @@ struct ContentView: View {
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
-
+    @State private var deg = 0.0
+    @State private var currentCountry = ""
+    @State private var opacity = 1.0
+    @State private var hasOffset = false
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing : 30){
-                   Text("Tap the flag of")
+                Text("Tap the flag of")
                     .foregroundColor(.white)
-                   Text(countries[correctAnswer])
+                Text(countries[correctAnswer])
                     .foregroundColor(.white)
                     .font(.largeTitle)
                     .fontWeight(.black)
                 
-                ForEach(0 ..< 3) { number in
-                    Button(action: {
-                        self.flagTapped(number)
-                    }) {
-                        FlagImage(name: self.countries[number])
+                ForEach(countries[0...2], id: \.self) { country in
+                    if country == countries[correctAnswer] {
+                        FlagImage(name: country)
+                            .rotation3DEffect(
+                                Angle(degrees : deg),
+                                axis:(0,1,0))
+                            .onTapGesture {
+                                flagTapped(country)
+                            }
+                            .offset(y: hasOffset ? 280 : 0)
+                    } else {
+                        FlagImage(name: country)
+                            .opacity(opacity)
+                            .onTapGesture {
+                                flagTapped(country)
+                                withAnimation(.easeOut){
+                                    self.hasOffset = true
+                                }
+                            }
+                            .offset(x: hasOffset ? 300 : 0)
                     }
                 }
                 
@@ -41,31 +60,40 @@ struct ContentView: View {
                     .foregroundColor(.white)
                 Spacer()
             }
+            
         }
         .alert(isPresented: $showingScore) {
             //Challenge 1 add score "Your score is \(score)"
             //Challenge 3 show correct answer
-            Alert(title: Text(scoreTitle), message: Text("Wrong! That’s the flag of \(countries[correctAnswer])"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("\(scoreTitle)! " + (scoreTitle == "Wrong" ? "That’s the flag of \(currentCountry)" : "Good Job!")), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
+            
         }
     }
     
-    func flagTapped(_ number: Int) {
-        if number == correctAnswer {
+    func flagTapped(_ country : String) {
+        self.currentCountry = country
+        if currentCountry == countries[correctAnswer] {
             scoreTitle = "Correct"
-            //Challenge 1 add score
+            withAnimation{
+                self.deg += 360
+                self.opacity = 0.25
+            }
             score += 1
+            
         } else {
             scoreTitle = "Wrong"
         }
-
         showingScore = true
     }
     
     func askQuestion() {
+        scoreTitle = ""
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        self.opacity = 1.0
+        self.hasOffset = false
     }
 }
 
